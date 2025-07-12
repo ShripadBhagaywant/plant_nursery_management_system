316c30d4170973a4eaf94e70caa32ab75c80f132
ShripadBhagaywant <bhagaywantshri1234@gmail.com>
2025-07-12 20:32:24 +0530

    Update MailConfig.java

--- plant_nursery_management_system/src/main/java/com/project/plant_nursery_management_system/config/MailConfig.java
+++ plant_nursery_management_system/src/main/java/com/project/plant_nursery_management_system/config/MailConfig.java
@@ -17,8 +17,8 @@ public class MailConfig {
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
 
-        mailSender.setUsername("teachshri@gmail.com");
-        mailSender.setPassword("mwxj fteu tbga xmto");
+        mailSender.setUsername("{UsedUserEmail}");
+        mailSender.setPassword("{UsedUserAppPassword}");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
